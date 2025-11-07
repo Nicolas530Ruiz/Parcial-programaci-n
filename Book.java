e5c3003a04d3a371ced7e63a4fb7ae370e5f599c
Nicolas530Ruiz <andres.ruiz-sa@uniminuto.edu.co>
2025-11-07 00:49:02 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -9,3 +9,5 @@ public class Book extends Material {
         return String.format("%s | %s - %s (%d) [%s]", id, getType(), title, year, isAvailable() ? "Disponible" : "Prestado");
     }
 }
+
+git commit -m "Esto es una clase Java llamada Book que representa un libro en un sistema de biblioteca, heredando de Material y almacenando autor, año, y métodos para obtener su tipo y representación textual."
