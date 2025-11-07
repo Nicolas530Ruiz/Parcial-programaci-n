6271f1c6da0477d170e680b4c398070867eb0dfb
Nicolas530Ruiz <andres.ruiz-sa@uniminuto.edu.co>
2025-11-07 00:58:04 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,3 +5,5 @@ public class Main {
         SwingUtilities.invokeLater(() -> new LibraryGUI().setVisible(true));
     }
 }
+
+git commit -m "Esto es la clase principal Main que inicia la aplicación de biblioteca ejecutando la interfaz gráfica en el hilo de eventos de Swing. ¿que es swing? Swing es una biblioteca de componentes gráficos para Java que permite crear interfaces de usuario (ventanas, botones, menús) de forma multiplataforma."
