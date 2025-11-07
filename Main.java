c36f2680fed186bf32ba8b9c570592897f56403d
Nicolas530Ruiz <andres.ruiz-sa@uniminuto.edu.co>
2025-11-07 00:30:47 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-package com.library;
-import javax.swing.SwingUtilities;
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LibraryGUI().setVisible(true));
-    }
-}
